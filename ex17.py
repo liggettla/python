9ec2885aa885d49d0e6ccac884f9410071f7ad6f
liggettla <alexander.liggett@gmail.com>
2015-06-29 18:50:49 -0600

    Rewrote the code

--- ex17.py
+++ ex17.py
@@ -2,20 +2,21 @@
 from sys import argv
 from os.path import exists
 
-#run scipt like this: python ex17.py test17.txt new_file17.txt
+#run like this: python ex17.py test17.txt new_file17.txt
 script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
 
-#we could do thes two on one line too, how?
-in_file = open(from_file)
-indata = in_file.read()
+#we could do these two in one line too, how?
+#in_file = open(from_file)
+#indata = in_file.read()
 
-#len() here gives the size of the file in bytes
+indata = open(from_file).read()
+
 print "The input file is %d bytes long" % len(indata)
 
 print "Does the output file exist? %r" % exists(to_file)
-print "ready, hit RETURN to continue, CTRL-C to abort."
+print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
 out_file = open(to_file, 'w')
@@ -24,4 +25,4 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
-in_file.close()
+#in_file.close()    #Only needed if line 14 is run as two lines as shown in lines 11 and 12
