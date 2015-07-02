cc4e8f4b51a5cb4fd658d29a98fb324666925717
liggettla <alexander.liggett@gmail.com>
2015-07-01 19:04:52 -0600

    Update comments

--- ex35.py
+++ ex35.py
@@ -52,13 +52,13 @@ def cthulhu_room():
 
 def dead(why):
     print why, "Good job!"
-    exit(0)
+    exit(0) #0 exits without any errors, a number greater than 0 indicates a particular error upon exiting
 
 def start():
     print "You are in a dark room."
     print "There is a door to your right and left."
     print "Which one do you take?"
-    
+
     next = raw_input("> ")
 
     if next == "left":
