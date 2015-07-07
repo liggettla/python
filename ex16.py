c95fd6ee8cb28d7dec46f36aa5a4c9437a49af9b
liggettla <alexander.liggett@gmail.com>
2015-07-06 19:14:01 -0600

    Small code change

--- ex16.py
+++ ex16.py
@@ -36,8 +36,7 @@ target.write(line1)
 target.write("\n")
 target.write(line2)
 target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(line3 + "\n")    #Same as above but in a single line
 
 print "And finally, we close it."
 target.close()
