60aef3bdd72c986427f596bb9d7d811d0ac9d28a
liggettla <alexander.liggett@gmail.com>
2015-07-27 14:42:07 -0600

    Added Engine() class

--- ex45.py
+++ ex45.py
@@ -1,1 +1,18 @@
 #This is a game made in the style of ex43.py
+from sys import exit
+from random import randint
+
+class Engine(object):
+
+    def __init__(self, scene_map)
+        self.scene_map = scene_map
+
+    def play(self):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene-map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
