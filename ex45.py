5e1728994844f8916b5b1e5f2fd3777bc08fc5de
liggettla <alexander.liggett@gmail.com>
2015-07-27 16:22:38 -0600

    Character class and friends() function added

--- ex45.py
+++ ex45.py
@@ -1,18 +1,54 @@
 #This is a game made in the style of ex43.py
 from sys import exit
 from random import randint
+from random import choice #chooses random char in string
 
-class Engine(object):
-
-    def __init__(self, scene_map)
-        self.scene_map = scene_map
+class Engine():
 
     def play(self):
-        current_scene = self.scene_map.opening_scene()
-        last_scene = self.scene-map.next_scene('finished')
+        name = raw_input("Input Player Name: ")
+        player = Character(name)
+        print "\nPlayer Name: %s" % (player.name)
+        print "Player Attack: %i" % (player.attack)
+        print "Player Defense: %i" % (player.defense)
+        print "Player Speed: %i\n" % (player.speed)
 
-        while current_scene != last_scene:
-            next_scene_name = current_scene.enter()
-            current_scene = self.scene_map.next_scene(next_scene_name)
+        friendName = friends()
+        friend = Character(friendName)
+        print "\nYour Friend's Name: %s" % (friend.name)
+        print "Your Friend's Attack: %i" % (friend.attack)
+        print "Your Friend's Defense: %i" % (friend.defense)
+        print "Your Friend's Speed: %i\n" % (friend.speed)
 
-        current_scene.enter()
+class Character(object):
+
+    def __init__(self, object):
+        self.attack = randint(1,100)
+        self.defense = randint(1,100)
+        self.speed = randint(1,100)
+        self.name = object
+
+class Scene(object):
+    pass
+
+def friends():
+    capitals = 'QWRTYPSDFGHJKLZXVBNM'
+    vowels = 'aeiou'
+    consonants = 'qwrtpsdfghjklzxcvbnm'
+    name = ''
+
+    for i in range(randint(2,10)):
+        if i == 0:
+            letter = choice(capitals)
+            name = name + letter
+        elif i%2 == 1:
+            letter = choice(vowels)
+            name = name + letter
+        elif i%2 == 0:
+            letter = choice(consonants)
+            name = name + letter
+    return name
+
+
+game = Engine()
+game.play()
